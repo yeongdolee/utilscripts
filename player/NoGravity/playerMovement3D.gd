390267d706323c39199c3d1ef5a45c868076e8d8
yeongdo Lee <yeongdolee1018@gmail.com>
2025-12-27 00:39:19 +0900

    Changed SPEED variable to export. Test Complated.

--- player/NoGravity/playerMovement3D.gd
+++ player/NoGravity/playerMovement3D.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+@export_group("Player Movement Settings")
+@export var SPEED = 5.0
 
 func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
